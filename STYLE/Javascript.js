4b69cd7893c35a1a5e9f744da6f1e5dc77ea679e
Student: Garrett Parker <g.parker@monroeisd.us>
2020-11-13 13:49:55 -0500

    Implementing encryption to hide the url from snoopers

--- STYLE/Javascript.js
+++ STYLE/Javascript.js
@@ -1,62 +1,71 @@
-function passWord() {
-	var pass = accessCookie("accesscode")
-	if (checkCode( pass, "checkCookie") == "TRUE") {
-		directProtected();
-		return " ";
-	} else if (checkCode( pass, "checkCookie") == "LOCKED") {
-		alert('LOCKED');
-		return " ";
-	} else {
-		var pass = prompt('Enter Access Code','');
-		if (pass == "") {
-			location.reload();
-		} else if (checkCode( pass, "passWord") == "TRUE") {
-			directProtected();
-			return " ";
-		}
-		var pass = prompt('INCORRECT','');
-		if (checkCode( pass, "passWord") != "TRUE") {
-			createCookie("accesscode", "bfc160483cb0e2c3834a16e02ba45e2995e8f00fdcab3a22505d495ab9ec288b")
+/* Procted Access */
+
+function passCode() {
+	var code = accessCookie("accesscode");
+	if (code != "") {
+		if (code == "LOCKED") {
 			alert('LOCKED');
 			return " ";
 		}
-		if (checkCode( pass, "passWord") == "TRUE") {
-			directProtected();
+		var pass = deCode(code)
+		alert(pass);
+		if (validURL(pass)) {
+			window.open(pass, '_self');
+			alert('GRANTED');
 			return " ";
 		}
+	}else {
+		var code = prompt('Enter Access Code','');
+		var pass = deCode(code);
+		if (code == "") {
+			return;
+		}
+		if (validURL(pass)) {
+			createCookie("accesscode", code)
+			window.open(pass, '_self');
+			alert('GRANTED');
+			return;
+		}
+		var code = prompt('INCORRECT','');
+		var pass = deCode(code);
+		if (validURL(pass)) {
+			createCookie("accesscode", code)
+			window.open(pass, '_self');
+			alert('GRANTED');
+			return;
+		} else {
+			createCookie("accesscode", "LOCKED")
+			alert('LOCKED');
+			return;
+		}
 	}
-	return " ";
+	return;
 }
 
-function checkCode(string, code){
-	switch(code) {
-		case "passWord":
-			if (SHA256(string) == "05f721989a4f70756a3b8387767affd11c776a0c863f1a22410855e606753321")
-				return "TRUE";
-			if (SHA256(string) == "bfc160483cb0e2c3834a16e02ba45e2995e8f00fdcab3a22505d495ab9ec288b")
-				return "LOCKED";
-			break;
-		case "checkCookie":
-			if (string == "05f721989a4f70756a3b8387767affd11c776a0c863f1a22410855e606753321")
-				return "TRUE";
-			if (string == "bfc160483cb0e2c3834a16e02ba45e2995e8f00fdcab3a22505d495ab9ec288b")
-				return "LOCKED";
-			break;
-	  default:
-		return "FALSE";
-	}
-	return " ";
+/* PROTECTED LINK U2FsdGVkX1+TIzQZ24AOi2t/mxZAwlTUpVNBCj4q/YxUGePN8oPiw51OMNpP1fBVlOPB5KmOwehmRvPFcL/xM1X+dUAery6VFzaIS7Vq72Y=
+	LOCKED */
+
+function deCode(code){
+	var secret = "U2FsdGVkX1+TIzQZ24AOi2t/mxZAwlTUpVNBCj4q/YxUGePN8oPiw51OMNpP1fBVlOPB5KmOwehmRvPFcL/xM1X+dUAery6VFzaIS7Vq72Y=";
+	var decode = CryptoJS.AES.decrypt(secret, code).toString(CryptoJS.enc.Utf8);
+	return decode;
 }
 
-function directProtected() {
-	createCookie("accesscode", "05f721989a4f70756a3b8387767affd11c776a0c863f1a22410855e606753321");
-	window.open('https://garrettspage.com/OTHER/PROTECTED/Protected-Hub.html', '_self');
-	alert('GRANTED');
+function validURL(str) {
+  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return !!pattern.test(str);
 }
+
+/* Encrypted Cookie Handler */
 
 function createCookie(cookieName,cookieValue)
 {
-  document.cookie = SHA256(cookieName) + "=" + cookieValue + "; expires=" + new Date(2147483647 * 1000).toUTCString(); + "; path=/; samesite=lax;";
+  document.cookie = SHA256(cookieName) + "=" + CryptoJS.AES.encrypt(cookieValue, "garrettspage") + "; expires=" + new Date(2147483647 * 1000).toUTCString(); + "; path=/; samesite=lax;";
 }
 
 function accessCookie(cookieName) {
@@ -66,7 +75,7 @@ function accessCookie(cookieName) {
   {
 	var temp = allCookieArray[i].trim();
 	if (temp.indexOf(name)==0)
-	return temp.substring(name.length,temp.length);
+	return CryptoJS.AES.decrypt(temp.substring(name.length,temp.length), "garrettspage").toString(CryptoJS.enc.Utf8);
   }
 	return "";
 }
