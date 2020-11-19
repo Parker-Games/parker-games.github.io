11e03380a9e6a9260679df935c03754148ec4450
Student: Garrett Parker <g.parker@monroeisd.us>
2020-11-19 14:10:21 -0500

    Header now imports

---
+++ STYLE/Header.js
@@ -0,0 +1,26 @@
+document.write(`
+	<div class="navigation">
+		<div class="brand">
+			<div class="inline-block">
+				<a href="https://garrettspage.com/index.html" >GARRETT PARKER</a>
+			</div>
+		</div>
+		<nav class="nav-block">
+			<div class="nav-link" style="width: 120px">
+				<div class="inline-block">
+					<a href="https://garrettspage.com/PROJECTS/Project-Hub.html" >PROJECTS</a>
+				</div>
+			</div>
+			<div class="nav-link" style="width: 90px">
+				<div class="inline-block">
+					<a href="https://garrettspage.com/MEMES/Meme-Hub.html" >MEMES</a>
+				</div>
+			</div>
+			<div class="nav-link-big" style="width: 150px">
+				<div class="inline-block">
+					<a href="https://garrettspage.com/OTHER/Info.html" >INFO/CONTACT</a>
+				</div>
+			</div>
+		</nav>
+	</div>
+`);
