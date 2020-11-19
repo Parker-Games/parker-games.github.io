4d8d409a2ec63f4cb58e6d1439b0ce512eece1ca
Garrett Parker <g.parker@monroeisd.us>
2020-11-19 17:39:44 -0500

    Update Footer.js

--- Style/Footer.js
+++ Style/Footer.js
@@ -1,1 +1,7 @@
-document.write(`	<div class="footer-copyright">		<div class="inline-block">			<a onClick="javascript:window.open('https://private.garrettspage.com', '_self')">© 1863 Garrett Parker</a>		</div>	</div>`);
+document.write(`
+	<div class="footer-copyright">
+		<div class="inline-block">
+			<a onClick="javascript:window.open('https://private.garrettspage.com', '_self')">Â© 1863 Garrett Parker</a>
+		</div>
+	</div>
+`);
