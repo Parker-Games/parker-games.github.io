d5a9ac8d07b9c060bac102c8555412a065485abe
Student: Garrett Parker <g.parker@monroeisd.us>
2020-11-19 17:31:37 -0500

    Imported Footer

---
+++ Style/Footer.js
@@ -0,0 +1,1 @@
+document.write(`	<div class="footer-copyright">		<div class="inline-block">			<a onClick="javascript:window.open('https://private.garrettspage.com', '_self')">© 1863 Garrett Parker</a>		</div>	</div>`);
